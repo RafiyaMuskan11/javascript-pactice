5990e7d0223f8b02f8124e0854fa9aea3f7628ee
RafiyaMuskan11 <rafiyamuskan11@gmail.com>
2022-06-25 08:44:34 +0530

    javascript pactice

---
+++ script.js
@@ -0,0 +1,34 @@
+//  1.primitive sata types
+//    a. string
+   var name = "muskan"
+   console.log(typeof name, name);
+
+//    b. number
+    var num = 15;
+    console.log(typeof num, num);
+
+
+//    c. boolean
+     var present = true;
+     console.log(typeof present, present);
+
+
+//     d. null 
+       var object = null;
+       console.log(typeof object, object);
+
+//     e. undefined
+        var method = undefined;
+        console.log(typeof method, method);
+
+//     f. symbol
+    //    var texture = symbol();
+    //    console.log(typeof texture);
+
+//    g. corceian
+    var string = " RafiyaMuskan";
+    var a = 11;
+    var sum = string+a;
+    console.log(typeof sum,sum);
+
+    
